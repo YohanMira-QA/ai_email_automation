1e02f6a9f631ed4e810e9bf9ee3a1a0587add3f5
yohanmira <yamira3@misena.edu.co>
2026-02-24 14:52:40 -0500

    Code update v5

--- core/ai_classifier.py
+++ core/ai_classifier.py
@@ -1,31 +1,4 @@
-"""from openai import OpenAI
-from config import OPENAI_API_KEY, MODEL
 
-# Crear cliente usando la API key
-client = OpenAI(api_key=OPENAI_API_KEY)
-"""
-def classify_email(content):
-    prompt = f"""
-    Clasifica el siguiente email en una de estas categorías:
-    - Lead
-    - Support
-    - Invoice
-    - Spam
-
-    Email:
-    {content}
-    """
-"""
-    response = client.chat.completions.create(
-        model=MODEL,
-        messages=[
-            {"role": "user", "content": prompt}
-        ],
-        temperature=0
-    )
-
-    return response.choices[0].message.content.strip()
-"""
 def classify_email(content):
     content = content.lower()
 
